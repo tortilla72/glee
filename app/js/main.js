3a34c66ff66e668b8eaeb3e118a5eea096444abf
Eshamova <eshamova72@yandex.ru>
2023-03-05 22:34:35 +0300

    add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-  
+$(function () {
+
+  $('.user-nav__btn-menu').unbind('click').on('click', function() {
+    $('.user-nav__menu-item-add').toggleClass('user-nav__menu-item-add--hidden');
+    return false;
+  });
+
+  $('.menu__list-btn').unbind('click').on('click', function(){
+    $(this).next().toggleClass('menu__dropdown-list--active');
+  });
 });

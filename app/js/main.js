1b8b39fa1a8807c52c6b2ac360858f8b6723a216
Eshamova <eshamova72@yandex.ru>
2023-03-07 14:20:15 +0300

    add footer

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,12 @@ $(function () {
 
   $('.user-nav__btn-menu').unbind('click').on('click', function() {
     $('.user-nav__menu-item-add').toggleClass('user-nav__menu-item-add--hidden');
-    return false;
   });
 
-  $('.menu__list-btn').unbind('click').on('click', function(){
-    $(this).next().toggleClass('menu__dropdown-list--active');
+  $('.menu__list-item--dropdown').hover(function() {
+    $(this).find('.menu__dropdown-list').addClass('menu__dropdown-list--active');
+  }, function() {
+    $(this).find('.menu__dropdown-list').removeClass('menu__dropdown-list--active');
   });
+
 });
